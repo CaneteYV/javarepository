1396be4f54883adfd496cdc0495bb8a03c05c5c6
Yvhan E. Canete <yvhaneno112108@gmail.com>
2026-06-17 14:43:01 +0800

    06/17/26

--- Basic_Structure_and_Syntax/main.java
+++ Basic_Structure_and_Syntax/main.java
@@ -1,29 +1,7 @@
 package Basic_Structure_and_Syntax;
 
 public class main { 
-     public static void main(String[] args) {
-        
-        System.out.println("Hello, World!"); 
-        System.out.println("Welcome to Java programming!");
-        System.out.println("Im Phosphophyllite, nice to meet you!");
-        System.out.println(136635150433l);
-        System.out.println("Im 400 years old, but I look like a 14 year old!");
-        System.out.println("See you after 10,000 years!");
-        System.out.println("I have the power to send you to a place filled with nothing but anguish and suffering,");
-        System.out.println("But I refuse to do that. Farewell.");
-        System.out.println("Addition");//this is addition
-        System.out.println("5 + 3 = " + (5 + 3));
-        System.out.println("Subtraction");//this is subtraction
-        System.out.println("5 - 3 = " + (5 - 3));
-        System.out.println("Multiplication");//this is multiplication
-        System.out.println("5 * 3 = " + (5 * 3));
-        System.out.println("Division");//this is division
-        System.out.println("5 / 3 = " + (5 / 3));
-        
-
-        
-    }    
-
+   
 
     
 }
