f7cbc4423fe79b46832b7fefb26f03e1e295a75b
Yvhan E. Canete <yvhaneno112108@gmail.com>
2026-06-17 14:34:20 +0800

    o

---
+++ Basic_Structure_and_Syntax/HelloWorld.java
@@ -0,0 +1,9 @@
+package Basic_Structure_and_Syntax;
+// This is a simple Java program that prints "Hello, World!" to the console. 
+
+public class HelloWorld { 
+    //this is the class declaration
+    // the name of the class is HelloWorld  System.out.println("Hello, World!"); 
+}
+    
+
