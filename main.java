3ac1bc35934d021bcb7c79cba586abbf84ecfc76
Yvhan E. Canete <yvhaneno112108@gmail.com>
2026-06-17 13:39:55 +0800

    Add files via upload

--- main.java
+++ main.java
@@ -8,7 +8,7 @@ public class main {
         System.out.println("Im 400 years old, but I look like a 14 year old!");
         System.out.println("See you after 10,000 years!");
         System.out.println("I have the power to send you to a place filled with nothing but anguish and suffering,");
-        System.out.println("But I refuse to do that. Farewell.");
+        System.out.println("But I refuse to do that. Farewellll.");
              
 
         
