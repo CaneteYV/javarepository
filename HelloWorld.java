63ffbe761b6ccc6cddb3d8fa926c2f1141c8a803
Yvhan E. Canete <yvhaneno112108@gmail.com>
2026-06-17 13:10:03 +0800

    gg

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+package Basic_Structure_and_Syntax;
+// This is a simple Java program that prints "Hello, World!" to the console. 
+
+public class HelloWorld { 
+    //this is the class declaration
+    // the name of the class is HelloWorld
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }    
+}
+
---
+++ main.java
@@ -0,0 +1,19 @@
+package Basic_Structure_and_Syntax;
+
+public class main { 
+     public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to Java programming!");
+        System.out.println("Im Phosphophyllite, nice to meet you!");
+        System.out.println("Im 400 years old, but I look like a 14 year old!");
+        System.out.println("See you after 10,000 years!");
+        System.out.println("I have the power to send you to a place filled with nothing but anguish and suffering,");
+        System.out.println("But I refuse to do that. Farewell.");
+             
+
+        
+    }    
+
+
+    
+}
