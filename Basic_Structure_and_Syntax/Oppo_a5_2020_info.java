6d0e3faaf110da26add099a7fbe0b06efb422050
Yvhan E. Canete <yvhaneno112108@gmail.com>
2026-06-18 14:45:06 +0800

    variableactivity06/18/26

---
+++ Basic_Structure_and_Syntax/Oppo_a5_2020_info.java
@@ -0,0 +1,22 @@
+package Basic_Structure_and_Syntax;
+
+public class Oppo_a5_2020_info {
+     public static void main(String [] args) {
+
+        String phoneModel = "Oppo a5 2020";
+        System.out.println("Phone Model: " + phoneModel);
+        int releasedDate = 2019;
+        System.out.println("Released Date: October " + releasedDate);
+        float batteryCapacity = 4800.900f;
+        System.out.println("Battery Capacity (mAh): " + batteryCapacity + " mAh");
+        char chargingPortType = 'c';
+        System.out.println("Charging Port Type: Type " + chargingPortType);
+        boolean goodBudgetPhone = true;
+        System.out.println("Good Budget Phone?: " + goodBudgetPhone);
+
+
+
+
+    }
+    
+}
