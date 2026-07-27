a18da1e3342bc6fa61c4426dd8fb473f5889f226
Yvhan E. Canete <yvhaneno112108@gmail.com>
2026-07-27 13:41:42 +0800

    07/27/26

---
+++ IF ELSE and SWITCH/ifElse.java
@@ -0,0 +1,40 @@
+package operation;
+
+public class ifElse {
+    public static void main(String[] args) {
+
+    int volume = 85;
+
+        if (volume == 0) {
+            System.out.println("Muted");
+        } else if (volume <= 20) {
+            System.out.println("Quiet");
+        } else if (volume <= 50) {
+            System.out.println("Moderate");
+        } else if (volume <= 80) {
+            System.out.println("Loud");
+        } else {
+            System.out.println("Warning: Volume level out of bounds!");
+        }
+
+
+         int drink = 1;
+        switch (drink) {
+            case 1:
+                System.out.println("Tea");
+                break;
+            case 2:
+                System.out.println("Coffee");
+                break;
+            case 3:
+                System.out.println("Juice");
+                break;
+            case 4:
+                System.out.println("Milk tea");
+                break;
+            default:
+                System.out.println("Water");
+                break;
+        }
+        }
+    }
